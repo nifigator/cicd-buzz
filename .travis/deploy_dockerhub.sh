d135f5d853c5972fe2f7de8bf33abbcd4d3a1e04
Boris Pokhozhaev <nifigator@gmail.com>
2019-06-02 18:07:44 +0700

    modify deploy to dockerhub

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t cicd-buzz:$TAG .
-docker push nifigator/cicd-buzz
-
+#!/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push USER/REPO
